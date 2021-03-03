b6fc9287f2f623402697b31a2f2263a9e0ee9e14
Jihoon Kim <jihoonerd@outlook.com>
2021-03-03 14:11:53 +0900

    Add test code for LSTM train

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,33 @@
 from mg.model.lstm import LSTMModel
 import torch
 import numpy as np
+from torch import nn
+import torch.optim as optim
 
-def test_model():
+def test_model_train():
     mat = np.load('npz_data/quaternion/F01A0V1.npz')
     input_mat = np.expand_dims(np.concatenate([mat['pos'], mat['rot']], axis=1), axis=0)
-    model = LSTMModel()
-    h0 = torch.randn((1,1,128))
-    c0 = torch.randn((1,1,128))
     input_mat = torch.Tensor(input_mat)
-    out, (hn, cn) = model(input_mat, (h0, c0))
+    
+    X = input_mat[:, :-1, :]
+    Y = input_mat[:, 1:, :]
+
+    model = LSTMModel()
+    loss_function = nn.L1Loss()
+    optimizer = optim.Adam(params=model.parameters())
+    # test output w/o training
+    num_epochs = 10
+    loss_hist = []
+    for i in range(num_epochs):
+        optimizer.zero_grad()
+        output = model(X)
+        pred = output.view(-1, output.size(2))
+        target = Y.view(-1, Y.size(2))
+        assert pred.shape == target.shape
+        loss = loss_function(pred, target)
+        loss.backward()
+        optimizer.step()
+        loss_hist.append(loss.item())
+    assert loss_hist[0] > loss_hist[-1]
+
+
